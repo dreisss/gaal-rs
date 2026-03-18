3f3265f027ddbfc8a996460ad3b47e01503ea4e6
Davi Reis <dreisss.dev@gmail.com>
2026-03-17 21:10:07 -0300

    feat(models/matrix): add tests module

---
+++ src/models/matrix.rs
@@ -0,0 +1,28 @@
+struct Matrix {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_print() {}
+
+    #[test]
+    fn test_access() {}
+
+    #[test]
+    fn test_add() {}
+
+    #[test]
+    fn test_scalar_multiply() {}
+
+    #[test]
+    fn test_matrix_multiply() {}
+
+    #[test]
+    fn test_transpose() {}
+
+    #[test]
+    fn test_inverse() {}
+
+    #[test]
+    fn test_determinant() {}
+}
